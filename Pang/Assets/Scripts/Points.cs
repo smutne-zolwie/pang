086268d30b213fadadaff5b6a4124d87850aec45
AreczeQ192 <askupien8@gmail.com>
2022-03-03 17:22:29 +0100

    Update(but with bug)

--- Pang/Assets/Scripts/Points.cs
+++ Pang/Assets/Scripts/Points.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Points : MonoBehaviour
 {
@@ -9,14 +11,22 @@ public class Points : MonoBehaviour
     public Collider2D[] bcollider;
     Map time;
     Controller heart;
+    public TMP_Text Tpoints;
+    public TMP_Text Timer;
 
     public void AddHeartAndTimePoints()
     {
-        //dziala
-        heart = GameObject.FindGameObjectWithTag("Player").GetComponent<Controller>();
+        heart = GameObject.FindGameObjectWithTag("Player").GetComponent<Controller>(); //problem gdy gracz nie zyje
         points += heart.hearts * 250;
     
         time = GameObject.FindGameObjectWithTag("Map").GetComponent<Map>();
         points += ((int)time.timer) * 50;
+    }
+
+    private void Update()
+    {
+        time = GameObject.FindGameObjectWithTag("Map").GetComponent<Map>();
+        Tpoints.text = points.ToString();
+        Timer.text = ((int)time.timer).ToString();
     }
 }
